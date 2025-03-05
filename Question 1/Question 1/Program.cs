b86a2c2d8568857360056388d7a3296cf3373075
ducgky <nghung28.vnu@gmail.com>
2025-03-06 00:09:50 +0700

    Complete Code

--- Question 1/Question 1/Program.cs
+++ Question 1/Question 1/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Question_1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            //B1_Nhập số từ bàn phím
+            Console.WriteLine("Nhập Số Thứ Nhất:  ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Nhập Số Thứ Hai:  ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //B2_Tính Toán Giá Trị 
+            double sum = number1 + number2;
+            double different = number1 - number2;
+            double product = number1 * number2;
+
+            Console.WriteLine("Kết quả của tổng là " + sum);
+            Console.WriteLine("Kết quả của hiệu là " + different);
+            Console.WriteLine("Kết quả của nhân là " + product);
+
+            //Kết Thúc
+            Console.WriteLine("Thoát Phép Tính");
+            Console.ReadKey();
+
         }
     }
 }
