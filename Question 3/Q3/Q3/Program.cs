be0db5e647b11fb511ce6ceddefca5cbf7b6e89e
ducgky <nghung28.vnu@gmail.com>
2025-03-06 01:29:00 +0700

    Complete

---
+++ Question 3/Q3/Q3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.InputEncoding = System.Text.Encoding.UTF8;
+                Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+                //Bắt đầu định nghĩa giá trị nhiệt độ
+                double c, k, f;
+
+                //Nhập độ C để đổi sang F và K
+                Console.WriteLine(" Nhiệt Độ C:");
+                c = float.Parse(Console.ReadLine());
+
+                //Tính Toán Đổi Độ C
+                k = c + 237.15 ;
+
+                f = (c * (1.8)) + 32;
+                //Hiển Thị
+                Console.WriteLine("Nhiệt Độ K Là: {0} ", k);
+                Console.WriteLine("Nhiệt Độ F Là: {0}  ", f);
+
+                Console.ReadLine();
+
+                break;
+            }
+            
+
+
+        }
+    }
+}
